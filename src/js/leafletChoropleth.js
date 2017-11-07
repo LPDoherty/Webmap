6d034b286107d2e3cc1c93bbede769815869b58c
LPDoherty <33264916+LPDoherty@users.noreply.github.com>
2017-11-07 12:26:52 -0500

    Update leafletChoropleth.js

--- src/js/leafletChoropleth.js
+++ src/js/leafletChoropleth.js
@@ -33,12 +33,12 @@ info.addTo(map);
 // get color depending on population density value
 function getColor(d) {
   return d > 1000 ? '#800026' :
-    d > 500 ? '#BD0026' :
-    d > 200 ? '#E31A1C' :
-    d > 100 ? '#FC4E2A' :
-    d > 50 ? '#FD8D3C' :
-    d > 20 ? '#FEB24C' :
-    d > 10 ? '#FED976' :
+    d > 70000 ? '#BD0026' :
+    d > 65000 ? '#E31A1C' :
+    d > 55000 ? '#FC4E2A' :
+    d > 50000 ? '#FD8D3C' :
+    d > 35000 ? '#FEB24C' :
+    d > 10000 ? '#FED976' :
     '#FFEDA0';
 }
 // set color of the json based on its Population Density
